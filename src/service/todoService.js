4dadc456f3c63cd942d1685a58a5e8a3d6796379
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2019-05-19 11:19:26 +0200

    firebase config

--- src/service/todoService.js
+++ src/service/todoService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class TodoService {
   constructor(){
     this.api = axios.create({
-      baseURL: 'http://localhost:4000',
+      baseURL: process.env.REACT_APP_BACKEND_URL,
       withCredentials: true
     })
   }
